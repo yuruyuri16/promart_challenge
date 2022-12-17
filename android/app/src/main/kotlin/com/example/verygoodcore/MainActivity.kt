9c4ac8adb25e78170525c85a6e871fe50775a840
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2022-12-16 19:57:27 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.promart_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:promart_challenge/app/app.dart';
+import 'package:promart_challenge/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
